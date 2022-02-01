47644153a92ccbc12c6712de8506675ee531bd86
Sasha <aleksander.tyutikov@mail.ru>
2022-02-01 23:02:23 +0800

    zmei

--- zmei.py
+++ zmei.py
@@ -1,37 +1,44 @@
-
 import pygame, time, random
 
 pygame.init()
 
-pygame.mixer.music.load('slugfest-2.wav')
-pygame.mixer.music.play(-1)
-# добавление змейки
-def snake(headname, bodyname, snakeList, snakeHead, lead_x, lead_y):
+
+# функция для отрисовки змейки
+def snake(block_size, snakeList):
     for XnY in snakeList:
-        gameDisplay.blit(pygame.image.load(bodyname),(XnY[0],XnY[1]))
-        gameDisplay.blit(pygame.image.load(headname),(lead_x,lead_y))
-# окошко
-def message_to_screen(msg,color,x,y):
+        pygame.draw.rect(gameDisplay, red, [XnY[0], XnY[1], block_size, block_size])
+
+
+# функция для отображения текста на экране
+def message_to_screen(msg, color, x, y):
     screen_text = font.render(msg, True, color)
-    gameDisplay.blit(screen_text, [x,y])
+    gameDisplay.blit(screen_text, [x, y])
 
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-green = (0,255,0)
 
-display_width = 600
-display_height = 600
-block_size = 40
-# счёт
-font = pygame.font.SysFont(None, 32)
-gameDisplay = pygame.display.set_mode((display_width,display_height))
+# задаем цвета заранее
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (255, 255, 0)
+orendg = (255, 165, 0)
+
+# задаем размеры окна и блоков змейки
+display_width = 400
+display_height = 400
+block_size = 25
+
+# создаем окно
+gameDisplay = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("Snake")
+
+font = pygame.font.SysFont(None, 25)
 
 gameExit = False
 
-lead_x = (display_width-40)/2
-lead_y = (display_height-40)/2
+# координаты головы змейки
+lead_x = display_width / 2
+lead_y = display_height / 2
+# переменные, отвечающие за направление движения змейки
 lead_x_change = 0
 lead_y_change = 0
 
@@ -39,8 +46,12 @@ snakeList = []
 snakeLength = 1
 score = 0
 
-appleX = round(random.randrange(block_size, display_width-block_size)/block_size)*block_size
-appleY = round(random.randrange(block_size, display_height-block_size)/block_size)*block_size
+# случайные координаты яблока
+appleX = round(random.randrange(0, display_width - block_size) / block_size) * block_size
+appleY = round(random.randrange(0, display_height - block_size) / block_size) * block_size
+
+orendgX = round(random.randrange(0, display_width - block_size) / block_size) * block_size
+orendgY = round(random.randrange(0, display_height - block_size) / block_size) * block_size
 
 while not gameExit:
     for event in pygame.event.get():
@@ -60,9 +71,12 @@ while not gameExit:
                 lead_y_change = -block_size
                 lead_x_change = 0
 
-    if lead_x >= display_width-block_size or lead_x <0 or lead_y >= display_height-block_size or lead_y <0:
+    gameDisplay.fill(white)
+
+    # столкновение со стеной
+    if lead_x >= display_width - block_size or lead_x < 0 or lead_y >= display_height - block_size or lead_y < 0:
         gameDisplay.fill(white)
-        message_to_screen(''.join(["Game over! Score: ",str(score)]), white, 200,260)
+        message_to_screen(''.join(["Game over! Score: ", str(score)]), black, 100, 200)
         pygame.display.update()
         time.sleep(2)
         gameExit = True
@@ -78,27 +92,34 @@ while not gameExit:
     # столкновение змейки с самой собой
     for eachSegment in snakeList[:-1]:
         if eachSegment == snakeHead:
-            gameDisplay.blit(pygame.image.load('bg.jpg'), (0, 0))
-            message_to_screen(''.join(["Game over! Score: ",str(score)]), white, 200,260)
+            gameDisplay.fill(white)
+            message_to_screen(''.join(["Game over! Score: ", str(score)]), black, 100, 200)
             pygame.display.update()
             time.sleep(2)
             gameExit = True
 
-    # столкновение с ЯБЛОКОМ
+    # столкновение с яблоком
     if lead_x == appleX and lead_y == appleY:
-        appleX = round(random.randrange(block_size, display_width-block_size)/block_size)*block_size
-        appleY = round(random.randrange(block_size, display_height-block_size)/block_size)*block_size
+        appleX = round(random.randrange(0, display_width) / block_size) * block_size
+        appleY = round(random.randrange(0, display_height) / block_size) * block_size
         snakeLength += 1
         score += 1
-    
-    # фон
-    gameDisplay.blit(pygame.image.load('bg.png'), (0, 0))
+
+    if lead_x == orendgX and lead_y == orendgY:
+        orendgX = round(random.randrange(0, display_width) / block_size) * block_size
+        orendgY = round(random.randrange(0, display_height) / block_size) * block_size
+        snakeLength += 1
+        score += 2
+
     # отображение количества очков
-    message_to_screen(''.join(["Score: ",str(score)]), white, 10,10)
-    # отображение яблока
-    gameDisplay.blit(pygame.image.load('apple.png'),(appleX, appleY))
+    message_to_screen(''.join(["Score: ", str(score)]), black, 10, 10)
+    # отображение яблока и апельсина
+    pygame.draw.rect(gameDisplay, green, [appleX, appleY, block_size, block_size])
+
+    pygame.draw.rect(gameDisplay, orendg, [orendgX, orendgY, block_size, block_size])
     # отображение змейки
-    snake('head.png', 'body.png', snakeList, snakeHead, lead_x, lead_y)
+    snake(block_size, snakeList)
+
     pygame.display.update()
     pygame.time.delay(150)
 
